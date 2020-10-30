d7ee6a8a595b1e94a14fcd342f0e4c1d816562a1
priyanshj89 <priyansh20010@gmail.com>
2020-10-30 08:39:23 +0530

    es6/es7 syntax, exceptions, delete using id

--- app.js
+++ app.js
@@ -2,15 +2,30 @@ const express = require('express');
 const crud = require('./routes/crud');
 const cors = require('cors');
 const logMiddleWare = require('./logMiddleware');
+const domain = require('domain');
 
 const app = express(); //invoking express
 const PORT = process.env.PORT || 5000;
+var d = domain.create();
+
+//one way og handling but not a good practice
+/*process.on('uncaughtException', err => {
+	// Handle the error safely
+	console.log(err);
+});
+*/
 
 app.use(cors());
 app.use(logMiddleWare);
 app.use(express.json()); //order matters, first parse then routing
 app.use(crud);
 
-app.listen(PORT, () => {
-	console.log('Server has started on port ', PORT);
+d.on('error', () => {
+	console.log(err);
+});
+
+d.run(() => {
+	app.listen(PORT, () => {
+		console.log('Server has started on port ', PORT);
+	});
 });
--- routes/crud.js
+++ routes/crud.js
@@ -3,36 +3,34 @@ const data = require('../data');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+	//arrow function syntax
 	//Sends students entire data
 	try {
-		res.status(200).send(data);
+		if (!data) {
+			res.status(400).json({ error: 'Error' });
+		} else {
+			res.status(200).send(data);
+		}
 	} catch (err) {
 		res.status(400).send('Error found, please try again');
 	}
 });
 
-/*router.get('/friend/:id', (req, res) => {
-	//find friend by id
-	data.friends
-		.find(req.params.id)
-		.then(data => {
-			res.status(200).json({ id: req.params.id, name: data.name });
-		})
-		.catch(err => {
-			console.log(`Error found ${err}`);
-			res.status(400).send(`Could not find the particular id ${err}`);
-		});
-});
-*/
-
 router.post('/addfriend', (req, res) => {
 	//adds a students friend
 	try {
-		data.friends.push({
+		/*data.friends.push({
 			id: req.body.id,
 			name: req.body.name
-		});
-		res.status(200).send(data.friends);
+		});*/
+		if (!req.body.id || !req.body.name) {
+			//checking vulnerability in data
+			res.status(400).json({ error: 'Error , please provide correct fields' });
+		} else {
+			data.friends = [...data.friends, { id: req.body.id, name: req.body.name }];
+			//using spread operator instead of push
+			res.status(200).send(data.friends);
+		}
 	} catch (err) {
 		console.log(`Error found ${err}`);
 		res.status(400).send(`Error found ${err}`);
@@ -42,8 +40,12 @@ router.post('/addfriend', (req, res) => {
 router.put('/favfruit', (req, res) => {
 	//updates students favorite fruit
 	try {
-		data.favoriteFruit = req.body.fruit;
-		res.status(200).json({ favouritefruit: data.favoriteFruit });
+		if (!data.favoriteFruit) {
+			res.status(400).json({ error: 'Error , please provide correct fields' });
+		} else {
+			data.favoriteFruit = req.body.fruit;
+			res.status(200).json({ favouritefruit: data.favoriteFruit });
+		}
 	} catch (err) {
 		console.log(`Error found ${err}`);
 		res.status(400).send(`Error found ${err}`);
@@ -53,11 +55,45 @@ router.put('/favfruit', (req, res) => {
 router.delete('/deletefriend', (req, res) => {
 	//deletes students last one friend
 	try {
-		data.friends.pop();
-		res.status(200).send(data.friends);
+		if (!data.friends) {
+			res.status(400).json({ error: 'Error' });
+		} else {
+			data.friends.pop();
+			res.status(200).send(data.friends);
+		}
 	} catch (err) {
 		console.log(`Error found ${err}`);
 		res.status(400).send(`Error found ${err}`);
+	}
+});
+
+router.delete('/delfriend/:id', (req, res) => {
+	//delete friend using url params
+	try {
+		if (!req.params.id) {
+			res.status(400).json({ error: 'Error , please provide correct fields' });
+		} else {
+			data.friends = data.friends.filter(friend => friend.id != req.params.id);
+			res.status(200).json(data.friends);
+		}
+	} catch (err) {
+		console.log(`Error found ${err}`);
+		res.status(400).send(`Could not find the particular id ${err}`);
+	}
+});
+
+router.delete('/delfriend/', (req, res) => {
+	//delete friend using query
+	try {
+		if (!req.query.id) {
+			res.status(400).json({ error: 'Error , please provide correct fields' });
+		} else {
+			data.friends = data.friends.filter(friend => friend.id != req.query.id);
+			res.status(200).json(data.friends);
+		}
+	} catch (err) {
+		console.log(`Error found ${err}`);
+		res.status(400).send(`Could not find the particular id ${err}`);
 	}
 });
 
